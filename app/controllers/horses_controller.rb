0714bfc2e0d78289cddd4e8393e3c0bd06c20181
julieconnors <juliana.g.connors@gmail.com>
2020-11-29 06:22:10 -0500

    add horse index page

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,11 +1,16 @@
 class HorsesController < ApplicationController
+    
+    def index
+        @user = User.find(session[:user_id])
+        #current_user
+    end
+
     def new
         #current_user
         @user = User.find(session[:user_id])
     end
 
     def create
-        #raise params.inspect
         @user = User.find(session[:user_id])
         horse = @user.horses.build(horse_params)
         horse.save
