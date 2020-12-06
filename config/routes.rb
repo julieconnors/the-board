5c9460ade1514c337e4ab47668ef92148ba77bbf
julieconnors <juliana.g.connors@gmail.com>
2020-12-06 18:12:41 -0500

    add routes for calendar search function

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   get "logout", to: "sessions#destroy"
+  get "ride_calendar", to: "rides#calendar"
+  post "ride_by_date", to: "rides#date"
 
   get "/auth/google_oauth2/callback", to: "sessions#omniauth"
   
@@ -12,6 +14,5 @@ Rails.application.routes.draw do
   end
   resources :rides
   resources :users
-
   resources :horses
 end
