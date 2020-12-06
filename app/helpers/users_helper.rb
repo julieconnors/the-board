b2e23ebd7a14493c28db96bf945d572908f5e3b8
julieconnors <juliana.g.connors@gmail.com>
2020-12-06 18:09:53 -0500

    add validation and error generator methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,24 @@
 module UsersHelper
     def user_validation(user)
-        # if user.save
-        # elsif 
-        # end
+        if user.save
+            session[:user_id] = user.id
+
+            redirect_to user_path(user)
+        else 
+            user_error_generator(user)
+            redirect_to "/register"
+        end
     end
 
-    def user_error_generator
-        
+    def user_error_generator(user)
+        if user.name == ""
+            flash[:name_error] = "Please add name"
+        end
+        if user.email == ""
+            flash[:email_error] = "Please add email"
+        end
+        if user.password == nil
+            flash[:password_error] = "Please add a password"
+        end
     end
 end
