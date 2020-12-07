c3594d27f4c17107e0b4e2f444f288f3b144a9cb
julieconnors <juliana.g.connors@gmail.com>
2020-12-07 14:13:04 -0500

    add helper method to validate data from edit form

--- app/helpers/riders_helper.rb
+++ app/helpers/riders_helper.rb
@@ -6,4 +6,17 @@ module RidersHelper
             flash[:error] = "Please add a name"
         end
     end
+
+    def edit_rider_validation(rider)
+        if rider.valid?
+            rider.save
+
+            redirect_to rider_path(rider)
+        else
+            @rider = rider
+            flash[:error] = "Please add a name"
+
+            render :edit
+        end
+    end
 end
