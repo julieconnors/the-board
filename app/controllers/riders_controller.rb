150beea14cf0f5833f653ddda2a065d674fae3e0
julieconnors <juliana.g.connors@gmail.com>
2020-12-09 21:17:24 -0500

    add rider destroy functionality

--- app/controllers/riders_controller.rb
+++ app/controllers/riders_controller.rb
@@ -2,6 +2,7 @@ class RidersController < ApplicationController
     include RidersHelper
 
     def index
+        @rider = Rider.find_by(id: params[:id])
     end
 
     def new
@@ -28,6 +29,14 @@ class RidersController < ApplicationController
         rider.update(name: params[:rider][:name])
         
         edit_rider_validation(rider)
+    end
+
+    def destroy
+        rider = Rider.find(params[:id])
+        rider.rides.map{|ride| ride.destroy }
+        Rider.destroy(params[:id])
+
+        redirect_to riders_path
     end
 
     private
