5f38166c70495efd1d4b9697d29ce57dc35c757b
julieconnors <juliana.g.connors@gmail.com>
2020-12-09 21:13:49 -0500

    add uniqueness validation

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -2,6 +2,7 @@ class Horse < ApplicationRecord
     has_many :rides
     has_many :riders, through: :rides
     belongs_to :user
-    validates_presence_of :name, :owner
+    validates_presence_of :owner
+    validates :name, uniqueness: true, presence: true
 
 end
