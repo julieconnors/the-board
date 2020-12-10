d9f5ad9e8de7e44b5bd983909aa8639a5577bde3
julieconnors <juliana.g.connors@gmail.com>
2020-12-09 21:21:14 -0500

    update format and add new horse form to index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 
     def current_user
-        #User.find(session[:user_id])
         @current_user ||= User.find_by(id: session[:user_id])
     end
 
@@ -11,7 +10,7 @@ module ApplicationHelper
 
     def logged_out_redirection #checks if a user is not logged in and redirects to login page
         if !logged_in?
-          redirect_to "/login"
+          redirect_to root
         end
     end
 end
