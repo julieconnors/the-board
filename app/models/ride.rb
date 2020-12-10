37126fc8d92cac72b3a254c5fdad7adc4827c622
julieconnors <juliana.g.connors@gmail.com>
2020-12-09 21:11:33 -0500

    add horse attributes method

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,7 +1,9 @@
 class Ride < ApplicationRecord
     belongs_to :horse
     belongs_to :rider
+    belongs_to :user
     validates_presence_of :day, :time
+    #accepts_nested_attributes_for :horse
     
     def format_date
         self.day.strftime('%-m/%d/%y')
@@ -10,4 +12,12 @@ class Ride < ApplicationRecord
     def format_time
         self.time.strftime('%l:%M')
     end
+
+    # def horse_attributes=(horse_attributes)
+    #     horse = Horse.find_or_create_by(horse_attributes)
+    #         ##I need to be able to assign user_id
+    #         if horse.valid?
+    #             self.horse = horse
+    #         end
+    # end
 end
