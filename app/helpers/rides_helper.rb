155f73696ae4e299b61bf7ef34cbf333d46ac1c0
julieconnors <juliana.g.connors@gmail.com>
2020-12-09 21:13:16 -0500

    add helper method to find todays rides

--- app/helpers/rides_helper.rb
+++ app/helpers/rides_helper.rb
@@ -12,6 +12,18 @@ module RidesHelper
         end
     end
 
+    def update_ride_validation
+        if ride.valid?
+            ride.save
+
+            redirect_to user_path(current_user)
+        else
+            error_generator(ride)
+
+            render :update
+        end
+    end
+
     def error_generator(ride)
         if ride.time == nil
             flash[:time_error] = "Please select a ride time" 
@@ -35,5 +47,9 @@ module RidesHelper
 
     def format_date_from_params(params)
         params[:day].to_date.strftime('%-m/%d/%y')
+    end
+
+    def todays_rides
+        current_user.rides.where("day = ?", Date.today)
     end
 end
