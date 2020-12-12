041d201e82cf61852957104f8cbd31d6f1dbfcb9
julieconnors <juliana.g.connors@gmail.com>
2020-12-12 15:14:19 -0500

    update resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,18 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   get "logout", to: "sessions#destroy"
 
-  get "ride_calendar", to: "rides#calendar"
-  #post "ride_by_date", to: "rides#date"
+  #get "ride_calendar", to: "rides#calendar"
   post "ride_by_date", to: "users#show"
 
 
   get "/auth/google_oauth2/callback", to: "sessions#omniauth"
   
+  resources :riders
+
   resources :riders do 
     resources :rides, only: [:index, :new, :create]
   end
-  resources :rides
+  resources :rides, only: [:show, :edit, :update, :destroy]
   resources :users
   resources :horses
 end
