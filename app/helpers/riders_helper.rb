9f03d45d9ec561bbfa6416610d930b6b2bd8b018
julieconnors <juliana.g.connors@gmail.com>
2020-12-12 15:32:01 -0500

    update error messages

--- app/helpers/riders_helper.rb
+++ app/helpers/riders_helper.rb
@@ -2,8 +2,12 @@ module RidersHelper
     def rider_validation(rider)
         if rider.valid?
             rider.save
+
+            redirect_to riders_path
         else
-            flash[:error] = "Please add a name"
+            flash[:error] = "Please add a rider name"
+
+            redirect_to riders_path
         end
     end
 
@@ -14,7 +18,7 @@ module RidersHelper
             redirect_to rider_path(rider)
         else
             @rider = rider
-            flash[:error] = "Please add a name"
+            flash[:error] = "Please add a rider name"
 
             render :edit
         end
