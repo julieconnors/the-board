f49bc88f0d30d42bf405f214f8cbf3de5cde4cf6
julieconnors <juliana.g.connors@gmail.com>
2020-12-14 06:45:37 -0500

    remove error generator method

--- app/helpers/rides_helper.rb
+++ app/helpers/rides_helper.rb
@@ -6,8 +6,6 @@ module RidesHelper
 
             redirect_to user_path(current_user)
         else
-            error_generator(ride)
-
             render :new
         end
     end
@@ -18,18 +16,7 @@ module RidesHelper
 
             redirect_to user_path(current_user)
         else
-            error_generator(ride)
-
             render :update
-        end
-    end
-
-    def error_generator(ride)
-        if ride.time == nil
-            flash[:time_error] = "Please select a ride time" 
-        end
-        if ride.day == nil
-            flash[:day_error] = "Please select a date"
         end
     end
 
