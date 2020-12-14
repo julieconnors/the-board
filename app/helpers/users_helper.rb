d1dee18ad81f373ec6e71239a2004438ad4664bd
julieconnors <juliana.g.connors@gmail.com>
2020-12-14 06:46:17 -0500

    remove error generator and update validation method to render new route

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,20 +5,7 @@ module UsersHelper
 
             redirect_to user_path(user)
         else 
-            user_error_generator(user)
-            redirect_to "/register"
-        end
-    end
-
-    def user_error_generator(user)
-        if user.name == ""
-            flash[:name_error] = "Please add name"
-        end
-        if user.email == ""
-            flash[:email_error] = "Please add email"
-        end
-        if user.password == nil
-            flash[:password_error] = "Please add a password"
+            render :new
         end
     end
 end
