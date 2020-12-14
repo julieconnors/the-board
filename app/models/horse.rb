0411695e98479eee1bfa886111db3202200d2f45
julieconnors <juliana.g.connors@gmail.com>
2020-12-13 19:35:46 -0500

    add nickname validation

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -2,7 +2,7 @@ class Horse < ApplicationRecord
     has_many :rides
     has_many :riders, through: :rides
     belongs_to :user
-    validates_presence_of :owner
+    validates_presence_of :owner, :nickname
     validates :name, uniqueness: true, presence: true
 
 end
