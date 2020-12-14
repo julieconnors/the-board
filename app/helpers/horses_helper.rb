a0625e6805fcfc4e9c697ac1b34498dd3b4467a0
julieconnors <juliana.g.connors@gmail.com>
2020-12-13 19:34:46 -0500

    remove error generator and update validation methods

--- app/helpers/horses_helper.rb
+++ app/helpers/horses_helper.rb
@@ -5,9 +5,7 @@ module HorsesHelper
 
             redirect_to horses_path
         else
-            horse_error_generator(horse)
-
-            redirect_to horses_path
+            render :new
         end
     end
 
@@ -17,22 +15,7 @@ module HorsesHelper
 
             redirect_to horses_path
         else
-            horse_error_generator(horse)
-
             render :edit
         end
-    end
-
-    def horse_error_generator(horse)
-        if horse.name == ""
-            flash[:name_error] = "Please add horse name"
-        end
-        if horse.owner == ""
-            flash[:owner_error] = "Please add owner name"
-        end
-    end
-
-    def sort_horse_rides
-        #self.
     end
 end
