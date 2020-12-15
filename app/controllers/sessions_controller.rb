59d77c50929f7f5948d9a62e2cbc4671792f818d
julieconnors <juliana.g.connors@gmail.com>
2020-12-15 06:20:47 -0500

    update login action to display errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,29 @@
 class SessionsController < ApplicationController
 
     def new
+        @user = User.new
     end
 
     def create
         @user = User.find_by(email: params[:email])
-        if @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
-        else
-            redirect_to "/login"
-        end
+            
+            if @user.nil?
+                @user = User.new
+                @user.valid?
+                @user.errors.messages[:email] = "We could not find an account under that email"
+
+                render :new
+            else
+                if @user.authenticate(params[:password])
+                    session[:user_id] = @user.id
+
+                    redirect_to user_path(@user)
+                else
+                    @user.errors.messages[:password] = "Incorrect password"
+                    
+                    render :new
+                end
+            end
     end
 
     def destroy
@@ -24,6 +37,7 @@ class SessionsController < ApplicationController
             u.password = SecureRandom.hex
             u.name = auth[:info][:name] 
         end
+        
         session[:user_id] = @user.id
         redirect_to user_path(@user)
     end
