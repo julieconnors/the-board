d1c206f614c9dbe806b640315d5ca65339236fa0
julieconnors <juliana.g.connors@gmail.com>
2020-12-16 20:24:56 -0500

    add instance methods to sort rides

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -4,4 +4,11 @@ class Rider < ApplicationRecord
     has_many :horses, through: :rides
     validates :name, uniqueness: true, presence: true
 
+    def todays_rider_rides
+        self.rides.where("day = ?", Date.today).order(:time)
+    end
+
+    def sort_rider_rides
+        self.rides.order(:day)
+    end
 end
