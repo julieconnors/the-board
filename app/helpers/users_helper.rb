066195a1f0be7a364bd6d477db523678b721e44d
julieconnors <juliana.g.connors@gmail.com>
2020-12-17 06:14:06 -0500

    add helper methods to handle login action logic

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,30 @@ module UsersHelper
             render :new
         end
     end
+
+    def find_user(user)
+        @user = user
+        if @user.nil?
+            @user = User.new
+            @user.valid?
+            @user.errors.messages[:email] = "We could not find an account under that email"
+
+            render :new
+        else
+            authenticate_user(user)    
+        end
+    end
+
+    def authenticate_user(user)
+        @user = user
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+
+            redirect_to user_path(@user)
+        else
+            @user.errors.messages[:password] = "Incorrect password"
+            
+            render :new
+        end
+    end
 end
