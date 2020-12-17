ebf3de775c29581fdfdca8592441d04ceec7d580
julieconnors <juliana.g.connors@gmail.com>
2020-12-17 06:19:39 -0500

    add flash message to logged out redirection method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,9 @@ module ApplicationHelper
 
     def logged_out_redirection #checks if a user is not logged in and redirects to login page
         if !logged_in?
-          redirect_to root_path
+            flash[:logged_out] = "Please login or signup"
+            
+            redirect_to root_path
         end
     end
 end
