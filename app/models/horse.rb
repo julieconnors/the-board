79a4a6535b883edb9feb7af420886ae10ee88d1d
julieconnors <juliana.g.connors@gmail.com>
2020-12-19 19:05:52 -0500

    add uniqueness validation to nickname

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -2,8 +2,10 @@ class Horse < ApplicationRecord
     has_many :rides
     has_many :riders, through: :rides
     belongs_to :user
-    validates_presence_of :owner, :nickname
+    validates_presence_of :owner
     validates :name, uniqueness: true, presence: true
+    validates :nickname, uniqueness: true, presence: true
+
 
     def sort_horse_rides
         self.rides.order(:day)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
     has_many :riders
     has_many :rides
     has_many :horses
-    validates_presence_of :password
     validates :name, uniqueness: true, presence: true
     validates :email, uniqueness: true, presence: true
+    validates_presence_of :password
 
 end
